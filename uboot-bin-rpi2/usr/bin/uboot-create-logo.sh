097b81414810d796282d230b31dc6301c9041121
anyc <dev@kicherer.org>
2016-02-26 17:02:09 +0100

    added uboot-bin-rpi2

---
+++ uboot-bin-rpi2/usr/bin/uboot-create-logo.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# create a uboot logo from a jpg or png image
+#
+# Written by Mario Kicherer (http://kicherer.org)
+
+IN=$1
+OUT=splash.bmp
+
+EXT=${IN##*.}
+
+case "${EXT}" in
+	png) CONV=pngtopnm ;;
+	jpg) CONV=jpegtopnm ;;
+esac
+
+if ! which "${CONV}" > /dev/null; then
+	echo "error, ${CONV} not found"
+	exit 1
+fi
+
+CONVBIN=$(which "${CONV}")
+
+"${CONVBIN}" "${IN}" | ppmquant 220 | ppmtobmp -bpp 8 | gzip -9 > "${OUT}"
